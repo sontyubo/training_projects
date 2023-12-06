e9f32c3e8d5aa435a12130323c3a3fa8abd39bdf
sontyubo <sontyubo@gmail.com>
2023-12-06 15:24:35 +0900

    static修正

--- config/settings.py
+++ config/settings.py
@@ -125,9 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [str(BASE_DIR / 'accounts' / 'static') , str(BASE_DIR / 'shopTip' / 'static')]
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'accounts/static/'),
+    os.path.join(BASE_DIR, 'shopTip/static/')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
